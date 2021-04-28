745263a46095a8a5b49c133d385883c36bea1232
sigvaldm <sigvald@marebakken.com>
2021-04-28 13:57:35 +0200

    Cleaned up parser

--- summary.py
+++ summary.py
@@ -5,7 +5,7 @@
 import numpy as np
 from os.path import join as pjoin
 import argparse
-from aux import *
+from parsers import *
 from langmuir import *
 from pprint import PrettyPrinter
 
@@ -14,20 +14,5 @@ parser.add_argument('folder', type=str,
                     help='Folder to simulation runs')
 args = parser.parse_args()
 
-# vars = parse_xoopic_input(pjoin(args.folder, 'input.inp'))
-# dt = vars['timeStep']
-
-# vars = parse_xoopic(pjoin(args.folder, 'input.inp'))
-# contents = XoopicParser()(pjoin(args.folder, 'input.inp'))
-# parser('Species', 'name')
-# parser('Diagnostic', 'n_step')
-# parser('Control', 'dt')
-# eval_xoopic_region(contents)
-i=read_xoopic_input(pjoin(args.folder, 'input.inp'))
-PrettyPrinter().pprint(i)
-PrettyPrinter().pprint(i)
-
-
-# omega_pe = Electron(n=vars['nE'], T=vars['tEK']).omega_p
-
-# print(dt/omega_pe)
+config=parse_xoopic_input(pjoin(args.folder, 'input.inp'))
+PrettyPrinter().pprint(config)
