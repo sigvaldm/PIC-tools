82b5791d9a11c9b76f6fe622c1748de7a41ae7ab
sigvaldm <sigvald@marebakken.com>
2021-04-28 13:46:55 +0200

    Better parser

--- aux.py
+++ aux.py
@@ -2,8 +2,9 @@
 
 import re
 import sys
-from parsy import generate, regex, string, seq, letter, digit, char_from
+from parsy import regex, string, seq
 from collections import namedtuple
+from pprint import PrettyPrinter
 
 def parse_xoopic_input(filename):
     '''
@@ -65,66 +66,164 @@ def parse_xoopic_block(filename, blockname):
 
     return vars
 
-def parse_xoopic(filename):
+def dict_of_list(list_of_pairs):
+    '''
+    Creates a dictionary from a list of (key,value)-pairs.
+    However, to allow multiple identical keys, each entry
+    will be a list of values belonging to that key.
+    '''
+    dict_of_list = {}
+    for key, value in list_of_pairs:
+        if key not in dict_of_list:
+            dict_of_list[key] = []
+        dict_of_list[key].append(value)
+    return dict_of_list
 
-    with open(filename) as file:
-        code = file.read()
+class XoopicParser(object):
+    '''
+    A parser for XOOPIC input files. Example of use:
 
-    # Remove comments
-    code = re.sub('//.*', '', code)
+        file_contents = XoopicParser()(filename)
+        PrettyPrinter.pprint(file_contents)
+    '''
 
-    # Remove leading and trailing whitespaces (incl. empty lines)
-    code = re.sub(r'^\s*', '', code, flags=re.MULTILINE)
-    code = re.sub(r'\s*$', '', code, flags=re.MULTILINE)
+    def __init__(self):
 
-    spaces = regex(r'[ \t]*')
-    whitespace = regex(r'\s*')
-    horspaces = regex('r[ \t]*')
+        spaces = regex(r'[ \t]*')  # Excludes newline
+        whitespace = regex(r'\s*') # Includes newline
 
-    newline = string('\n')
-    lbrace  = whitespace << string('{') << whitespace
-    rbrace  = whitespace << string('}') << whitespace
-    equal   = string('=')
-    # word    = regex('[a-zA-Z0-9\-.,*_:%]+')
-    word    = regex('[^\s=}]+')
-    # words   = regex('[a-zA-Z0-9\-._: ]*')
-    words   = word + (spaces+word).many().concat()
+        newline = string('\n')
+        equal   = string('=')
+        lbrace  = whitespace << string('{') << whitespace
+        rbrace  = whitespace << string('}') << whitespace
 
-    # whitespace = regex(r'\s*')
-    # lexeme  = lambda p: whitespace >> p << whitespace
-    # lbrace  = lexeme(string('{'))
-    # rbrace  = lexeme(string('}'))
-    # equal   = lexeme(string('='))
-    # word    = lexeme((letter|digit|char_from('.-_%:')).at_least(1).concat())
+        # These parsers don't terminate blocks
+        word    = regex('[^\s=}]+')
+        words   = word + (spaces+word).many().concat()
+        characters = regex(r'[^}]*')
 
-    key_value_line = seq(spaces >> word << spaces << equal, spaces >> words << spaces)
-    key_value_dict = key_value_line.sep_by(newline).map(dict)
-    # key_value_dict = key_value_line.many().map(dict)
-    # key_value_dict = key_value_line.map(namedtuple).many()
-    # res = p.parse(' key =  value ')
-    # print(res)
+        key_value_line = seq(spaces >> word << spaces << equal, spaces >> words << spaces)
+        key_value_lines = key_value_line.sep_by(newline).map(dict)
 
-    block = seq(word << lbrace, key_value_dict << rbrace)
-    comment_block = seq(word << lbrace, regex(r'[^}]*') << rbrace)
-    # res = block.many().parse(s)
-    region_block = seq(whitespace >> string('Region') << lbrace, block.many() << rbrace)
-    variables_block = seq(whitespace >> string('Variables') << lbrace, regex(r'[^}]*') << rbrace)
-    plasmadiag_block = seq(whitespace >> string('Plasma_Diagnostics') << lbrace, regex(r'[^}]*') << rbrace)
-    xoopic_file = seq(plasmadiag_block, variables_block, region_block)
-    # xoopic_file = (variables_block | region_block | plasmadiag_block).many()
-    # res = region_block.parse(s)
-    # res = xoopic_file.parse_partial(s)
-    # for n, line in enumerate(code.split('\n')):
-    #     print(n, line)
-    res = xoopic_file.parse(code)
+        def block(name, content):
+            return seq(whitespace >> name, lbrace >> content << rbrace)
 
-    for key in res:
-        print(key[0])
-    # print(res[1]['temperature'])
+        key_value_block = block(word, key_value_lines)
+        key_value_blocks = key_value_block.many().map(dict_of_list)
+        region_block = block(string('Region'), key_value_blocks)
+        other_block = block(word, characters)
 
-    return res
+        self.parser = (region_block | other_block).many().map(dict)
+
+    def __call__(self, filename):
+
+        with open(filename) as file:
+            code = file.read()
+
+        # Remove comment
+        code = re.sub('//.*', '', code)
+
+        # Remove leading and trailing whitespaces (incl. empty lines)
+        code = re.sub(r'^\s*', '', code, flags=re.MULTILINE)
+        code = re.sub(r'\s*$', '', code, flags=re.MULTILINE)
+
+        # FIXME: parser should handle comments and whitespaces through
+        # combinators. Not ad-hoc.
+
+        return self.parser.parse(code)
+
+def eval_xoopic_region(xoopic_input):
+
+    # Import sqrt and other mathematical function into Variables block
+    code = xoopic_input['Variables']
+    code = 'from math import *\n' + code
+
+    # Execute as Python and store variables
+    variables = {}
+    exec(code, None, variables)
+
+    # Evaluate each value in the region block using the variables
+    region_block = xoopic_input['Region']
+    for subblock in region_block.values():
+        for parameters in subblock:
+            for key in parameters:
+                try:
+                    parameters[key] = eval(parameters[key], None, variables)
+                except:
+                    pass
+
+    return region_block
+
+def read_xoopic_input(filename):
+    '''
+    Parses and evaluates the contents of XOOPIC input file.
+    '''
+    contents = XoopicParser()(filename)
+    evaluated_contents = eval_xoopic_region(contents)
+    return evaluated_contents
 
 
+# def parse_xoopic_input2(filename):
+
+#     with open(filename) as file:
+#         code = file.read()
+
+#     # Remove comment
+#     code = re.sub('//.*', '', code)
+
+#     # Remove leading and trailing whitespaces (incl. empty lines)
+#     code = re.sub(r'^\s*', '', code, flags=re.MULTILINE)
+#     code = re.sub(r'\s*$', '', code, flags=re.MULTILINE)
+
+#     # FIXME: parser should handle comments and whitespaces
+
+#     return xoopic_parser().parse(code)
+
+
+# class XoopicParser(object):
+
+#     def __init__(self, filename):
+
+#         with open(filename) as file:
+#             code = file.read()
+
+#         # Remove comments
+#         code = re.sub('//.*', '', code)
+
+#         # Remove leading and trailing whitespaces (incl. empty lines)
+#         code = re.sub(r'^\s*', '', code, flags=re.MULTILINE)
+#         code = re.sub(r'\s*$', '', code, flags=re.MULTILINE)
+
+#         self._parse(code)
+#         self._eval_variables()
+
+#     def _parse(self, code):
+
+#         self.contents = xoopic_file.parse(code)
+
+#     def _eval_variables(self):
+
+#         # Import sqrt and other mathematical function
+#         code = self.contents['Variables']
+#         code = 'from math import *\n' + code
+
+#         # Execute as Python
+#         self.variables = dict()
+#         exec(code, None, self.variables)
+
+#     def __call__(self, section, key, evaluate=True):
+#         values = []
+#         for block in self.contents['Region']:
+#             if block[0] == section:
+#                 value = block[1][key]
+#                 if evaluate:
+#                     try:
+#                         value = eval(value, None, self.variables)
+#                     except:
+#                         pass
+#                 values.append(value)
+#         print(values)
+#         return values
 
 
     # blockname='Region'
--- summary.py
+++ summary.py
@@ -7,6 +7,7 @@ from os.path import join as pjoin
 import argparse
 from aux import *
 from langmuir import *
+from pprint import PrettyPrinter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('folder', type=str,
@@ -16,7 +17,16 @@ args = parser.parse_args()
 # vars = parse_xoopic_input(pjoin(args.folder, 'input.inp'))
 # dt = vars['timeStep']
 
-vars = parse_xoopic(pjoin(args.folder, 'input.inp'))
+# vars = parse_xoopic(pjoin(args.folder, 'input.inp'))
+# contents = XoopicParser()(pjoin(args.folder, 'input.inp'))
+# parser('Species', 'name')
+# parser('Diagnostic', 'n_step')
+# parser('Control', 'dt')
+# eval_xoopic_region(contents)
+i=read_xoopic_input(pjoin(args.folder, 'input.inp'))
+PrettyPrinter().pprint(i)
+PrettyPrinter().pprint(i)
+
 
 # omega_pe = Electron(n=vars['nE'], T=vars['tEK']).omega_p
 
